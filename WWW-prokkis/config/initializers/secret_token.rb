da3de38a1036efc923e0ecebbd71ea7eac87dfbb
Tommi Simell <tommi.simell@aalto.fi>
2012-04-06 16:02:57 +0300

    Rails app tehty

---
+++ WWW-prokkis/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WWWProkkis::Application.config.secret_token = 'cbaba8dc3fdb5846b09355c29c99aa82ce517b490459a2b829259d81951696aa6dc68a835a812c831ba525b4efb5542024a76aa2d98c4a491425d93a509fdac9'
