6353a8e84a19b734a911cbb81d998983336abfaa
Tommi Simell <tommi.simell@aalto.fi>
2012-04-09 15:55:00 +0300

    Madöfakkas

---
+++ WWW-prokkis/config/navigation.rb
@@ -0,0 +1,20 @@
+
+SimpleNavigation::Configuration.run do |navigation|
+  
+
+  navigation.items do |primary|
+
+    primary.item :kirjat, 'kirjat'
+	primary.item :autot, 'autot'
+	primary.item :aseet, 'aseet'
+	
+
+   
+   
+    end
+
+
+
+  end
+
+
