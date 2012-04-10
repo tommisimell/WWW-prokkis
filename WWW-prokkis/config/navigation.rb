360d531a43a643d0397358d2e95bee796f87d1ee
Tommi Simell <tommi.simell@aalto.fi>
2012-04-10 16:47:37 +0300

    Kuveja lissää

--- WWW-prokkis/config/navigation.rb
+++ WWW-prokkis/config/navigation.rb
@@ -5,9 +5,7 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
 
 	primary.item :home, 'Etusivu', home_index_path
-    primary.item :kirjat, 'kirjat', 'http://www.google.fi'
-
-	primary.item :aseet, 'aseet', 'http://www.puolustusvoimat.fi'
+	
 	primary.item :yhteystiedot, 'Yhteystiedot', pages_yhteystiedot_path do |yhteystiedot|
 		yhteystiedot.item :historia ,'Historia', pages_historia_path
 		yhteystiedot.item :palvelut ,'Palvelut', pages_palvelut_path
