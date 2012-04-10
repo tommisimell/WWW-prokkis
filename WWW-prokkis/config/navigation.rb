4fcdf9ee7654f8aa5ec5b542e5be270a2fd57628
Tommi Simell <tommi.simell@aalto.fi>
2012-04-10 15:09:37 +0300

    Navigööntipalkki toimii nyt, muokattu kaikennäköstä mut ulkoasusettii täytyy viel hieroo.

--- WWW-prokkis/config/navigation.rb
+++ WWW-prokkis/config/navigation.rb
@@ -4,12 +4,17 @@ SimpleNavigation::Configuration.run do |navigation|
 
   navigation.items do |primary|
 
-	primary.item :home, 'etusivu', home_index_path
+	primary.item :home, 'Etusivu', home_index_path
     primary.item :kirjat, 'kirjat', 'http://www.google.fi'
-	primary.item :posts, 'Postit', posts_path
+
 	primary.item :aseet, 'aseet', 'http://www.puolustusvoimat.fi'
-	primary.item :yhteystiedot, 'Yhteystiedot', pages_yhteystiedot_path
-	
+	primary.item :yhteystiedot, 'Yhteystiedot', pages_yhteystiedot_path do |yhteystiedot|
+		yhteystiedot.item :historia ,'Historia', pages_historia_path
+		yhteystiedot.item :palvelut ,'Palvelut', pages_palvelut_path
+		end
+	primary.item :posts, 'Postit', posts_path do |posts|
+		posts.item :lajiesittely ,'Lajiesittely', pages_lajiesittely_path
+		end
 	
 
    
